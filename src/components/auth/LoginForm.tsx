3cdff459bbc78d6d6a90ca26dec73c99c0367abb
Kamlesh Kumar <kkjha00007@gmail.com>
2025-05-23 18:59:41 +0000

    Change it more meaningful, "Don't have an account? Register here", I think it could be "New User?, Don't have an account? Register here"

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -101,7 +101,7 @@ export function LoginForm() {
           </form>
         </Form>
         <p className="mt-6 text-center text-sm text-muted-foreground">
-          Don't have an account?{' '}
+          New User?, Don&apos;t have an account?{' '}
           <Link href="/register" className="font-medium text-primary hover:underline">
             Register here
           </Link>
